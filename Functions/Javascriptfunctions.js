a4739ab100f8d3a6615caa46a4221cd875305eef
prati-personal <pratikshya.priyadarshini@gmail.com>
2026-05-22 00:09:59 +0530

    Changed the contents of the functions folder

---
+++ Functions/Javascriptfunctions.js
@@ -0,0 +1,28 @@
+//example-print
+/*function add(a,b)
+{
+    console.log(a+b);
+}
+add(10,3);*/
+
+//example: return the value
+/*function add(a,b)
+{
+    return(a+b);
+} 
+let result=add(10,30);
+console.log("Result",result); */
+
+//Example
+/*function greetings()
+{
+    console.log("Good Morning");
+}
+greetings();*/
+//Example
+function greetings()
+{
+   return("Good Morning");
+
+}
+console.log(greetings());
